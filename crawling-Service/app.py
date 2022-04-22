f8af91500a68c5632d6fae0c6f96a2aab0f2ccc4
조현아 <hyuna919@naver.com>
2022-04-22 15:01:26 +0900

    edit #S06P31E102-70 : POST요청 처리

--- crawling-Service/app.py
+++ crawling-Service/app.py
@@ -7,23 +7,29 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 
-# 메뉴 크롤링, post 로 식당 정보를 받아서 크롤링 실시
-@app.route('/crawling-service/delivery', methods=['POST'])
-def menu_crawling():
+# 메뉴 크롤링 체크
+@app.route('/crawling-service/delivery/<board_id>', methods=['GET'])
+def menu_crawling_check(board_id):
+    # Redis - get board_id
+    print("ccc")
     return jsonify()
 
 
 # 크롤링한 데이터 삭제
 @app.route('/crawling-service/delivery', methods=['DELETE'])
-def crawling_delete():
+def crawling_delete(board_id):
+    # Redis - del board_id
     return jsonify()
 
-
-# 메뉴 크롤링 체크
-@app.route('/crawling-service/delivery/<board_id>', methods=['GET'])
-def menu_crawling_check(board_id):
+# 메뉴 크롤링, post 로 식당 정보를 받아서 크롤링 실시
+@app.route('/crawling-service/delivery', methods=['POST'])
+def menu_crawling():
+    board_id = request.get_json()['board_id']
     server = yogiyo.UpdateMenu(file="./connection.txt")
     menus = server.menu_information(board_id)
+    # Redis 저장 - 키값(board_id)
+    # set board_id menus
+    print(menus)
     return jsonify(menus)
 
 
