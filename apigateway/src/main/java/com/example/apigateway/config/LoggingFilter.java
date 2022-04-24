9e2e1188d77f79b5f10b58ebaf2a3f30eca3895f
JangHyunJin <winckey3@gmail.com>
2022-04-24 19:48:00 +0900

    feat : gateway config 및 생성

---
+++ apigateway/src/main/java/com/example/apigateway/config/LoggingFilter.java
@@ -0,0 +1,51 @@
+package com.example.apigateway.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
+            //OrderedGatewayFilter : GatewayFilter인터페이스와 Orderd(순서저장)인터페이스 상속 OrderedGatewayFilter를이용해서 생성
+            //내부의 public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) 를 이용
+            //ServerWebExchange : mvc 패턴은 serlvet을 지원하지 않음 대신에 serverres 와 serverreq를 이용 이때 servel-> serverRes를 변환해줌
+            //
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
+            if (config.isPreLogger()) {
+                log.info("Logging PRE Filter: request id -> {}", request.getId());
+            }
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if (config.isPostLogger()) {
+                    log.info("Logging POST Filter: response code -> {}", response.getStatusCode());
+                }
+            }));
+        }, Ordered.LOWEST_PRECEDENCE);
+
+        return filter;
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
