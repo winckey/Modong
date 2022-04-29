acb716eaf222dc6922e31bfbed071a45bb36d99e
JIS <ygc8257@naver.com>
2022-04-29 09:25:16 +0900

    feat #S06P31E102-95: 게시판 ui

--- frontend/src/components/home/Delivery.tsx
+++ frontend/src/components/home/Delivery.tsx
@@ -6,15 +6,9 @@ import Modal from '../modal/DeliveryModal'
 const data = [{name:"오나라식탁", arrivepoint:"sk뷰 아파트 106동 1101호", lefttime:10}, {name:"오나라2식탁", arrivepoint:"sk뷰 아파트 106동 1102호", lefttime:20}]
 
 function delivery() {
-<<<<<<< HEAD:frontend/src/components/home/Delivery.js
     // const handleModalOpen = (data) =>{
     //     alert(data)
     // }
-=======
-    const handleModalOpen = (data:any) =>{
-        alert(data)
-    }
->>>>>>> feature/frontend/chat-ui:frontend/src/components/home/Delivery.tsx
     return (
         <div className='inList'>
             {data.map((d, index) =>(
@@ -22,12 +16,8 @@ function delivery() {
                     <div>{d.name}</div>
                     <div>{d.arrivepoint}</div>
                     <div>{d.lefttime}분 남았습니다.</div>
-<<<<<<< HEAD:frontend/src/components/home/Delivery.js
                     {/* <div onClick={()=>{handleModalOpen(data)}}>신청하기</div> */}
                     <Modal/>
-=======
-                    <div onClick={()=>{handleModalOpen(d)}}>신청하기</div>
->>>>>>> feature/frontend/chat-ui:frontend/src/components/home/Delivery.tsx
                 </div>
             ))}
         </div>
