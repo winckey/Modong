e391823236c2f889c0a7a58c26c81c07daf07b54
박신영 <sin5015243@naver.com>
2022-04-29 09:29:47 +0900

    feat #S06P31E102-89: 프로필 페이지 제작

--- frontend/src/components/mypage/Profile.js
+++ frontend/src/components/mypage/Profile.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import '../../style/_profile.scss'
+
 
 function Profile() {
     return (
-        <div>
-            <p> 프로필</p>
+        <div className="profile">
+
+            <div>
+                <button>수정</button>
+            </div>
+
+            <div>
+                <img src={ require('../../assets/dd.png') } alt="사진"/>
+                <button>+</button>
+            </div>
+
+            <div>
+                <div>
+                    <p>닉네임</p>
+                    <p>문현동 강냉이</p>
+                </div>
+                <div>
+                    <p>이메일 주소</p>
+                    <p>ssafy@naver.com</p>
+                </div>
+                <div>
+                    <p>주소</p>
+                    <p>구서동</p>
+                </div>
+                <div>
+                    <p>전화번호</p>
+                    <p>101-1234-9716</p>
+                </div>
+            </div>
+
         </div>
     );
 }
