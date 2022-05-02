e5cde727d6678a057df9535948a8704036fb76eb
JangHyunJin <winckey3@gmail.com>
2022-05-02 13:14:40 +0900

    etc #S06P31E102-121: swagger 연동

--- backend/apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
+++ backend/apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
@@ -1,10 +1,17 @@
 package com.example.apigateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import static java.util.stream.Collectors.toList;
 
 @SpringBootApplication
 public class ApigatewayApplication {
@@ -17,4 +24,21 @@ public class ApigatewayApplication {
         return new InMemoryHttpTraceRepository();
     }
 
+    @Bean
+    @Primary
+    public SwaggerResourcesProvider swaggerResourcesProvider(
+            DiscoveryClient discoveryClient,
+            @Value("${spring.application.name}") String gateway
+    ) {
+        return () -> discoveryClient.getServices()
+                .stream()
+                .filter(service -> !service.equals(gateway))
+                .map(service -> {
+                    var resource = new SwaggerResource();
+                    resource.setName(service);
+                    resource.setLocation(String.format("/%s/v3/api-docs", service));
+                    return resource;
+                })
+                .collect(toList());
+    }
 }
