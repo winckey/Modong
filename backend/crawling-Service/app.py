5d4f380e3ac56abda9701dd70c2559632fbe2675
조현아 <hyuna919@naver.com>
2022-05-04 16:26:49 +0900

    refactor  : swagger부착위한 flask-restx 사용에 따른 구조 변경
    클래스형으로 변동
    app.py파일을 controller.py로 분리

--- backend/crawling-Service/app.py
+++ backend/crawling-Service/app.py
@@ -1,10 +1,9 @@
-import json
-
 from flask import Flask, request, jsonify
-import redis
+from flask_restx import Api, Resource, reqparse
+
 import py_eureka_client.eureka_client as eureka_client
 
-import yogiyo
+from controller import Menu
 
 rest_port = 8080
 
@@ -19,39 +18,10 @@ eureka_client.init(eureka_server="http://k6e102.p.ssafy.io:8761/eureka",
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-rd = redis.StrictRedis(host='k6e102.p.ssafy.io', port=6379, db=0)
-
-
-# 메뉴 크롤링 체크
-@app.route('/crawling-service/delivery/<board_id>', methods=['GET'])
-def menu_crawling_check(board_id):
-    # Redis - get board_id
-    menus = rd.get(board_id)
-
-    return menus
-
-
-# 크롤링한 데이터 삭제
-@app.route('/crawling-service/delivery', methods=['DELETE'])
-def crawling_delete():
-    board_id = request.get_json()['board_id']
-    rd.delete(board_id)
-
-    return "DELETE COMPLETE"
-
-
-# 메뉴 크롤링, post 로 식당 정보를 받아서 크롤링 실시
-@app.route('/crawling-service/delivery', methods=['POST'])
-def menu_crawling():
-    board_id = request.get_json()['board_id']
-    server = yogiyo.UpdateMenu()
-    menus = json.dumps(server.menu_information(board_id), ensure_ascii=False).encode('utf-8')
-
-    # Redis 저장 - 키값(board_id)
-    rd.set(board_id, menus)
-
-    return menus
+api = Api(app, version='1.0', title='Crawling API 문서', description='Swagger 문서', doc="/api-docs")
+api.add_namespace(Menu, '/crawling-service/delivery')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=rest_port)
+    app.run()
+    # app.run(host='0.0.0.0', port=rest_port)
---
+++ backend/crawling-Service/controller.py
@@ -0,0 +1,52 @@
+import json
+
+from flask import request
+from flask_restx import Api, Resource, reqparse, Namespace, fields
+
+import redis
+
+import yogiyo
+
+rest_port = 8080
+
+# redis
+rd = redis.StrictRedis(host='k6e102.p.ssafy.io', port=6379, db=0)
+
+Menu = Namespace(
+    name="Menus",
+    description="Menu 크롤링 위해 사용하는 API.",
+)
+
+menu_fields = Menu.model('Menu', {  # Model 객체 생성
+    'board_id': fields.Integer(description='가게 고유번호')
+})
+
+@Menu.route('')
+class Crawling(Resource):
+    # 크롤링한 데이터 삭제
+    @Menu.expect(menu_fields)
+    def delete(self):
+        board_id = request.get_json()['board_id']
+        rd.delete(board_id)
+        return "DELETE COMPLETE"
+
+    # 메뉴 크롤링, post 로 식당 정보를 받아서 크롤링 실시
+    @Menu.expect(menu_fields)
+    def post(self):
+        board_id = request.get_json()['board_id']
+        server = yogiyo.UpdateMenu()
+        menus = json.dumps(server.menu_information(board_id), ensure_ascii=False).encode('utf-8')
+
+        # Redis 저장 - 키값(board_id)
+        rd.set(board_id, menus)
+
+        return menus.decode('utf-8')
+
+
+
+@Menu.route('/<int:board_id>')
+class Get(Resource):
+    # 메뉴 크롤링 체크
+    def get(self, board_id):
+        menus = rd.get(board_id)
+        return menus.decode('utf-8')
