ca41601825ad554c11ea7b8d6005016c09115913
조현아 <hyuna919@naver.com>
2022-05-10 16:06:07 +0900

    fix : localdatetime이 배열로 반환되는 문제 해결

--- backend/chatting-service/src/main/java/com/example/chattingservice/config/WebConfig.java
+++ backend/chatting-service/src/main/java/com/example/chattingservice/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+// @EnableWebMvc // dateformat
 public class WebConfig implements WebMvcConfigurer
 {
     @Override
