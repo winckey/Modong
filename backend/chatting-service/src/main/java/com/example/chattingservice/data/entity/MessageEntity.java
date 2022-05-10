cd47ca95c0d038bdde41884d2435bbbf9f0aed20
조현아 <hyuna919@naver.com>
2022-05-10 16:05:56 +0900

    refactor : 필요없는 import 제거

--- backend/chatting-service/src/main/java/com/example/chattingservice/data/entity/MessageEntity.java
+++ backend/chatting-service/src/main/java/com/example/chattingservice/data/entity/MessageEntity.java
@@ -1,11 +1,7 @@
 package com.example.chattingservice.data.entity;
 
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Builder
 @Getter
