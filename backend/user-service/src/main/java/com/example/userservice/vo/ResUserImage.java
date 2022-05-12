e57b3892e62bf81235d8406425e5ca75f35bfde5
JangHyunJin <winckey3@gmail.com>
2022-05-12 17:46:34 +0900

    edit

--- backend/user-service/src/main/java/com/example/userservice/vo/ResUserImage.java
+++ backend/user-service/src/main/java/com/example/userservice/vo/ResUserImage.java
@@ -1,11 +1,15 @@
 package com.example.userservice.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResUserImage {
 
     private MultipartFile multipartFile ;
