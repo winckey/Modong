284b083c6291eefa64c26f2c8740bc3777dc5cbf
JangHyunJin <winckey3@gmail.com>
2022-07-04 23:49:17 +0900

    Edit : order tset

---
+++ backend/order-serivce/src/test/java/com/modong/OrderTest/OrderTests.java
@@ -0,0 +1,18 @@
+package com.modong.orderserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    void Order 생성(){
+        Order order = Order.builder;
+    }
+}
